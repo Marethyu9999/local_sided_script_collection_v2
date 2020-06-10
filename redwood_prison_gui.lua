ee675d3613a63d62e39cc06ea109d54572a7ce8c
Marethyu9999 <66386137+Marethyu9999@users.noreply.github.com>
2020-06-10 12:25:12 +0200

    Add files via upload

---
+++ redwood_prison_gui.lua
@@ -0,0 +1,45 @@
+_G.WALKSPEED = 100
+_G.JUMPPOWER = 100
+_G.GUIDRAGSPEED = 5
+_G.MAINBACKGROUND = Color3.fromRGB(172, 176, 255)
+_G.HUBBUTTON = Color3.new(1, 1, 1)
+_G.TEXTANDBORDER = Color3.new(1,1,1)
+_G.BUTTONBACKGROUND = Color3.fromRGB(146, 138, 255)
+_G.HILIGHTLINES = Color3.new(1, 1, 1)
+_G.GUICLICKID = "903267862"
+_G.GUNSTOGGLEKEY = Enum.KeyCode.LeftAlt
+_G.MAINTOGGLEKEY = Enum.KeyCode.L
+_G.TELETOGGLEKEY = Enum.KeyCode.LeftAlt
+_G.EXTRTOGGLEKEY = Enum.KeyCode.LeftAlt
+_G.SKIN1SHIRTID = "1931228554"
+_G.SKIN1PANTSID = "1931229502"
+_G.SKIN2SHIRTID = "3130724146"
+_G.SKIN2PANTSID = "3739695749"
+_G.SKIN3SHIRTID = "1149750541"
+_G.SKIN3PANTSID = "1146539791"
+_G.guns = {
+"Taser",
+"M98B",
+"ACR",
+"L86A2",
+"Fake ID Card",
+"M60",
+"Parashute",}
+_G.toolsonspawn = {
+"Taser",
+"ACR",
+"Fake ID Card",}
+--GUN MENU SETTINGS
+_G.INFAMMO = true
+_G.INSTARELOAD = true
+_G.FASTFIRE = true
+_G.ALWAYSAUTO = true
+_G.INSTAKILL = false
+_G.GUN1 = "ACR"
+_G.GUN2 = "Taser"
+_G.GUN3 = "M60"
+_G.GUN4 = "M98B"
+_G.GUN5 = "L86A2"
+_G.GUN6 = "Dragunov"
+_G.GUN7 = "Minigun"
+loadstring(game:GetObjects('rbxassetid://5045457417')[1].Source)()
