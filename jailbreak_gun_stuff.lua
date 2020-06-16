3c34630e95df9516cfa6f7fdf7e07d160925ac22
Marethyu9999 <66386137+Marethyu9999@users.noreply.github.com>
2020-06-16 19:27:48 +0200

    Update jailbreak_gun_stuff.lua

--- jailbreak_gun_stuff.lua
+++ jailbreak_gun_stuff.lua
@@ -1,1 +1,11 @@
 
+for i,v in pairs(getgc(true))do
+       if type(v)=="table" and rawget(v,"Damage") then
+       v.MagSize=math.huge
+       v.FireFreq=100
+       v.BulletSpread=0
+       v.FireAuto=true
+       v.CamShakeMagnitude=0
+       v.BulletSpeed=100000
+   end
+end
